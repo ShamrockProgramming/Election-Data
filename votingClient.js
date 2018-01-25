7394aed69164b18396701d34b03a5d0eac3a6125
Eric <efleming@stpatrick.org>
2018-01-25 17:41:58 -0600

    largest voting difference function complete
    not surprising it was LA

--- votingClient.js
+++ votingClient.js
@@ -13,6 +13,13 @@ console.log(list);
 console.log(parsedList);
 */
 
+// find states with at least/at most/exactly blank counties
+// find all state totals aka fips ==0
+// find all states where percent difference was less than p%
+// sort counties within a given state from least to greatest total votes
+//x name counties a candidate won in a state and show percent.
+// which state had the county with the largest % difference
+// which state had the county with the largest literal vote difference
 
 //Will find the name and total votes between the two candidates.
 function stateTotals(datalist){
@@ -30,4 +37,31 @@ function stateTotals(datalist){
     return totals;
 }
 
+function largestVoteDiff(datalist){
+    let state;
+    let county;
+    let diff;
+    //find first valid things.
+    for(let x=1;x<datalist.length;x++){
+        if(datalist[x].fips != 0){
+            state = datalist[x].state;
+            county = datalist[x].county;
+            diff = Math.abs(datalist[x].obamaV - datalist[x].romneyV);
+            break;
+        }
+    }
+    //go through the whole list and update it as you go.
+    for(let x=2;x<datalist.length;x++){
+        let currentDiff = Math.abs(datalist[x].obamaV - datalist[x].romneyV);
+        if(currentDiff > diff && datalist[x].fips != 0){
+            diff = currentDiff;
+            county = datalist[x].county;
+            state = datalist[x].state;
+        }
+    }
+    console.log("the difference was "+diff+"\nin the state of "+state+"\nin "+county+" county");
+    return state;
+}
+
+console.log(largestVoteDiff(parsedList));
 //console.log(stateTotals(parsedList));
