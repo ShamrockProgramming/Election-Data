1a22b67bd204ab61dca7d6b7fbcb46d83fbbc6a5
Eric <efleming@stpatrick.org>
2018-01-25 17:26:55 -0600

    finished the state totals function
    Ironically this shows that there are 7 states missing from the data set, DC is included.

--- votingClient.js
+++ votingClient.js
@@ -7,5 +7,27 @@ let list = FR.LoadFileIntoArrayByLine('electionSimplified.csv');
 let Parser = new ElectionParser();
 let parsedList = Parser.all(list);
 
+/*
+//works
 console.log(list);
 console.log(parsedList);
+*/
+
+
+//Will find the name and total votes between the two candidates.
+function stateTotals(datalist){
+    let totals = new Set();
+    function state(obj){
+        this.name = obj.state;
+        this.total = obj.obamaV+obj.romneyV;
+    }
+    for(let r=0;r<datalist.length;r++){
+        if(datalist[r].fips === "0"){
+            totals.add(new state(datalist[r]));
+        }
+    }
+    console.log("The size of this set is "+totals.size);
+    return totals;
+}
+
+//console.log(stateTotals(parsedList));
